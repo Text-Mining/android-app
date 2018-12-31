56d00005956081cedddc57caa248dd54d0adbb40
Behrouz Khezry <bkhezry@gmail.com>
2018-12-31 16:05:30 +0330

    create Launcher activity for login page.

---
+++ app/src/main/java/com/github/bkhezry/persianner/LauncherActivity.java
@@ -0,0 +1,14 @@
+package com.github.bkhezry.persianner;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LauncherActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_launcher);
+  }
+}
