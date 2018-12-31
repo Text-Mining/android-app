28da18079f9abfc1a7d68ff8d15869178f6918ed
Behrouz Khezry <bkhezry@gmail.com>
2018-12-31 17:33:15 +0330

    create sign in function in APIService interface.

--- app/src/main/java/com/github/bkhezry/persianner/service/APIService.java
+++ app/src/main/java/com/github/bkhezry/persianner/service/APIService.java
@@ -1,4 +1,18 @@
 package com.github.bkhezry.persianner.service;
 
+import com.github.bkhezry.persianner.model.AuthInfo;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
 public interface APIService {
+
+  @POST("token")
+  @FormUrlEncoded
+  Call<AuthInfo> signIn(
+      @Field("Email") String email,
+      @Field("Password") String password);
+
 }
