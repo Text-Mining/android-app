4bedccabc2c8bfb0dd0daacb9d82f81fd0bbd726
Behrouz Khezry <bkhezry@gmail.com>
2018-12-31 18:15:21 +0330

    change mode of request to JsonObject.

--- app/src/main/java/com/github/bkhezry/persianner/service/APIService.java
+++ app/src/main/java/com/github/bkhezry/persianner/service/APIService.java
@@ -1,18 +1,15 @@
 package com.github.bkhezry.persianner.service;
 
 import com.github.bkhezry.persianner.model.AuthInfo;
+import com.google.gson.JsonObject;
 
 import retrofit2.Call;
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface APIService {
 
   @POST("auth/login")
-  @FormUrlEncoded
-  Call<AuthInfo> signIn(
-      @Field("Email") String email,
-      @Field("Password") String password);
+  Call<AuthInfo> signIn(@Body JsonObject signInData);
 
 }
