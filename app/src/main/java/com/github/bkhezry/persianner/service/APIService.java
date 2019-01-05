cfb5564701cb913bcc7d34ed7c2a3fe717d92b42
Behrouz Khezry <bkhezry@gmail.com>
2019-01-05 14:58:59 +0330

    Remove JsonObject request to server & replace with @Field.

--- app/src/main/java/com/github/bkhezry/persianner/service/APIService.java
+++ app/src/main/java/com/github/bkhezry/persianner/service/APIService.java
@@ -1,15 +1,16 @@
 package com.github.bkhezry.persianner.service;
 
 import com.github.bkhezry.persianner.model.AuthInfo;
-import com.google.gson.JsonObject;
 
 import retrofit2.Call;
-import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 public interface APIService {
 
   @POST("auth/login")
-  Call<AuthInfo> signIn(@Body JsonObject signInData);
-
+  @FormUrlEncoded
+  Call<AuthInfo> signIn(@Field("Email") String email,
+                        @Field("Password") String password);
 }
