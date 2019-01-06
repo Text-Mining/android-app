96a9ce1bcb0da2b8216c17d48b64735805f02d06
Behrouz Khezry <bkhezry@gmail.com>
2019-01-06 16:48:33 +0330

    add BoxStore instance creator to MyApplication.

--- app/src/main/java/com/github/bkhezry/persianner/util/MyApplication.java
+++ app/src/main/java/com/github/bkhezry/persianner/util/MyApplication.java
@@ -2,5 +2,23 @@ package com.github.bkhezry.persianner.util;
 
 import android.app.Application;
 
+import io.objectbox.BoxStore;
+import io.objectbox.android.AndroidObjectBrowser;
+import io.objectbox.android.BuildConfig;
+
 public class MyApplication extends Application {
+  private static BoxStore boxStore;
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    createBoxStore();
+  }
+
+  private void createBoxStore() {
+    boxStore = MyObjectBox.builder().androidContext(MyApplication.this).build();
+    if (BuildConfig.DEBUG) {
+      new AndroidObjectBrowser(boxStore).start(this);
+    }
+  }
 }
