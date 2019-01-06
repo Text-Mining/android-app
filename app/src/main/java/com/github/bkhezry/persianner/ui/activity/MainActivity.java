05e12f078ba8c3acc9783a805fc5dbd600b535d4
Behrouz Khezry <bkhezry@gmail.com>
2019-01-06 13:46:03 +0330

    add getSentence function to main activity.

--- app/src/main/java/com/github/bkhezry/persianner/ui/activity/MainActivity.java
+++ app/src/main/java/com/github/bkhezry/persianner/ui/activity/MainActivity.java
@@ -2,17 +2,24 @@ package com.github.bkhezry.persianner.ui.activity;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.bkhezry.persianner.R;
 import com.github.bkhezry.persianner.model.AuthInfo;
+import com.github.bkhezry.persianner.model.Sentence;
+import com.github.bkhezry.persianner.service.APIService;
 import com.github.bkhezry.persianner.util.Constant;
+import com.github.bkhezry.persianner.util.RetrofitUtil;
 import com.github.pwittchen.prefser.library.rx2.Prefser;
 import com.google.android.material.chip.Chip;
 import com.google.android.material.chip.ChipGroup;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,7 +39,21 @@ public class MainActivity extends AppCompatActivity {
   }
 
   private void getSentence() {
+    APIService apiService = RetrofitUtil.getRetrofit(authInfo.getToken()).create(APIService.class);
+    Call<Sentence> call = apiService.randomSentence();
+    call.enqueue(new Callback<Sentence>() {
+      @Override
+      public void onResponse(@NonNull Call<Sentence> call, @NonNull Response<Sentence> response) {
+        if (response.isSuccessful()) {
 
+        }
+      }
+
+      @Override
+      public void onFailure(@NonNull Call<Sentence> call, @NonNull Throwable t) {
+        t.printStackTrace();
+      }
+    });
   }
 
   private void generateChips() {
