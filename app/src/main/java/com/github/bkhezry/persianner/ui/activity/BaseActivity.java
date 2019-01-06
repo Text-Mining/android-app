62526c677dbb2e9feba1cb9088abd0544f193a49
Behrouz Khezry <bkhezry@gmail.com>
2019-01-07 02:44:34 +0330

    add set locale in BaseActivity. loading resources.

--- app/src/main/java/com/github/bkhezry/persianner/ui/activity/BaseActivity.java
+++ app/src/main/java/com/github/bkhezry/persianner/ui/activity/BaseActivity.java
@@ -4,12 +4,15 @@ import android.content.Context;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.bkhezry.persianner.util.MyApplication;
+
 import io.github.inflationx.viewpump.ViewPumpContextWrapper;
 
 public class BaseActivity extends AppCompatActivity {
 
   @Override
   protected void attachBaseContext(Context base) {
-    super.attachBaseContext(ViewPumpContextWrapper.wrap(base));
+    Context newContext = MyApplication.localeManager.setLocale(base);
+    super.attachBaseContext(ViewPumpContextWrapper.wrap(newContext));
   }
 }
