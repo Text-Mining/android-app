29dc738c747e74435f8b6335cf96055b3a5283cc
Behrouz Khezry <bkhezry@gmail.com>
2019-01-06 16:50:09 +0330

    add getter & setter to id field.

--- app/src/main/java/com/github/bkhezry/persianner/model/NerStandardTagsItem.java
+++ app/src/main/java/com/github/bkhezry/persianner/model/NerStandardTagsItem.java
@@ -26,6 +26,14 @@ public class NerStandardTagsItem {
   @SerializedName("Title")
   private String title;
 
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
   public String getPersianName() {
     return persianName;
   }
