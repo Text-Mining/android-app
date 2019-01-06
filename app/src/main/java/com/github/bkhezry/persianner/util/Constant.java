846e75de8129dfed131c0fa5fe56df411e97995b
Behrouz Khezry <bkhezry@gmail.com>
2019-01-07 02:23:32 +0330

    add layoutDirection to all layout with value locale

--- app/src/main/java/com/github/bkhezry/persianner/util/Constant.java
+++ app/src/main/java/com/github/bkhezry/persianner/util/Constant.java
@@ -4,5 +4,4 @@ public class Constant {
   public static final String BASE_URL = "https://app.text-mining.ir/api/";
   public static final String AUTH_INFO = "auth-info";
   public static final String SIGN_UP_URL = "https://app.text-mining.ir/Account/Register";
-  public static final String TAG_INFO = "tag-info";
 }
