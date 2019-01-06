01be411e56dadbd5992b35826e914c4b8857a8d1
Behrouz Khezry <bkhezry@gmail.com>
2019-01-06 13:39:37 +0330

    get auth info from SharedPreferences.

--- app/src/main/java/com/github/bkhezry/persianner/ui/activity/MainActivity.java
+++ app/src/main/java/com/github/bkhezry/persianner/ui/activity/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.bkhezry.persianner.R;
+import com.github.bkhezry.persianner.model.AuthInfo;
+import com.github.bkhezry.persianner.util.Constant;
+import com.github.pwittchen.prefser.library.rx2.Prefser;
 import com.google.android.material.chip.Chip;
 import com.google.android.material.chip.ChipGroup;
 
@@ -15,12 +18,16 @@ public class MainActivity extends AppCompatActivity {
 
   @BindView(R.id.chip_group)
   ChipGroup chipGroup;
+  private Prefser prefser;
+  private AuthInfo authInfo;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     ButterKnife.bind(this);
+    prefser = new Prefser(this);
+    authInfo = prefser.get(Constant.AUTH_INFO, AuthInfo.class, null);
     getSentence();
   }
 
