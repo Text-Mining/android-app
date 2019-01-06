92948a12770c12beae63e16ed2795a4179f7adac
Behrouz Khezry <bkhezry@gmail.com>
2019-01-06 13:38:24 +0330

    create chip from layout.

--- app/src/main/java/com/github/bkhezry/persianner/ui/activity/MainActivity.java
+++ app/src/main/java/com/github/bkhezry/persianner/ui/activity/MainActivity.java
@@ -5,12 +5,32 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.bkhezry.persianner.R;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+  @BindView(R.id.chip_group)
+  ChipGroup chipGroup;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    ButterKnife.bind(this);
+    getSentence();
+  }
+
+  private void getSentence() {
+
+  }
+
+  private void generateChips() {
+    Chip chip =
+        (Chip) getLayoutInflater().inflate(R.layout.item_chip, chipGroup, false);
+    chipGroup.addView(chip);
   }
 }
