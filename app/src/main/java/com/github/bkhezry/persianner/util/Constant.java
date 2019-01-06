c93a8e0df900cc52120311d0f9dd48c8bf697770
Behrouz Khezry <bkhezry@gmail.com>
2019-01-06 14:13:11 +0330

    change URL of register

--- app/src/main/java/com/github/bkhezry/persianner/util/Constant.java
+++ app/src/main/java/com/github/bkhezry/persianner/util/Constant.java
@@ -3,5 +3,5 @@ package com.github.bkhezry.persianner.util;
 public class Constant {
   public static final String BASE_URL = "https://app.text-mining.ir/api/";
   public static final String AUTH_INFO = "auth-info";
-  public static final String SIGN_UP_URL = "https://app.text-mining.ir/Account/Login?ReturnUrl=%2FCustomerPanel";
+  public static final String SIGN_UP_URL = "https://app.text-mining.ir/Account/Register";
 }
