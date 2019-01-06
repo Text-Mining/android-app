b8bb33381e9ee5daa76e2f6dca4c5efd94d151df
Behrouz Khezry <bkhezry@gmail.com>
2019-01-06 17:51:51 +0330

    getting color of each word from local db & show.

--- app/src/main/java/com/github/bkhezry/persianner/ui/activity/MainActivity.java
+++ app/src/main/java/com/github/bkhezry/persianner/ui/activity/MainActivity.java
@@ -1,15 +1,20 @@
 package com.github.bkhezry.persianner.ui.activity;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 
 import com.github.bkhezry.persianner.R;
 import com.github.bkhezry.persianner.model.AuthInfo;
+import com.github.bkhezry.persianner.model.NerStandardTagsItem;
 import com.github.bkhezry.persianner.model.Sentence;
 import com.github.bkhezry.persianner.model.WordsItem;
 import com.github.bkhezry.persianner.service.APIService;
+import com.github.bkhezry.persianner.util.AppUtil;
 import com.github.bkhezry.persianner.util.Constant;
+import com.github.bkhezry.persianner.util.MyApplication;
 import com.github.bkhezry.persianner.util.RetrofitUtil;
 import com.github.pwittchen.prefser.library.rx2.Prefser;
 import com.google.android.material.chip.Chip;
@@ -17,6 +22,8 @@ import com.google.android.material.chip.ChipGroup;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.objectbox.Box;
+import io.objectbox.BoxStore;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -27,6 +34,7 @@ public class MainActivity extends BaseActivity {
   ChipGroup chipGroup;
   private Prefser prefser;
   private AuthInfo authInfo;
+  private Box<NerStandardTagsItem> tagsItemBox;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +43,8 @@ public class MainActivity extends BaseActivity {
     ButterKnife.bind(this);
     prefser = new Prefser(this);
     authInfo = prefser.get(Constant.AUTH_INFO, AuthInfo.class, null);
+    BoxStore boxStore = MyApplication.getBoxStore();
+    tagsItemBox = boxStore.boxFor(NerStandardTagsItem.class);
     getSentence();
   }
 
@@ -68,6 +78,7 @@ public class MainActivity extends BaseActivity {
     Chip chip =
         (Chip) getLayoutInflater().inflate(R.layout.item_chip, chipGroup, false);
     chip.setText(wordsItem.getWord());
+    chip.setChipBackgroundColor(ColorStateList.valueOf(Color.parseColor(AppUtil.colorHexString(wordsItem.getTag(), tagsItemBox))));
     return chip;
   }
 }
