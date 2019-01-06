5c66ed42a069fd24c3fd62b91dcfdfc057277cfc
Behrouz Khezry <bkhezry@gmail.com>
2019-01-06 16:26:04 +0330

    create TagInfo class.

---
+++ app/src/main/java/com/github/bkhezry/persianner/model/NerStandardTagsItem.java
@@ -0,0 +1,61 @@
+package com.github.bkhezry.persianner.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class NerStandardTagsItem {
+
+  @SerializedName("PersianName")
+  private String persianName;
+
+  @SerializedName("Description")
+  private String description;
+
+  @SerializedName("Shortcut")
+  private String shortcut;
+
+  @SerializedName("Color")
+  private String color;
+
+  @SerializedName("Title")
+  private String title;
+
+  public String getPersianName() {
+    return persianName;
+  }
+
+  public void setPersianName(String persianName) {
+    this.persianName = persianName;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getShortcut() {
+    return shortcut;
+  }
+
+  public void setShortcut(String shortcut) {
+    this.shortcut = shortcut;
+  }
+
+  public String getColor() {
+    return color;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+}
---
+++ app/src/main/java/com/github/bkhezry/persianner/model/TagInfo.java
@@ -0,0 +1,19 @@
+package com.github.bkhezry.persianner.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class TagInfo {
+
+  @SerializedName("NerStandardTags")
+  private List<NerStandardTagsItem> nerStandardTags;
+
+  public List<NerStandardTagsItem> getNerStandardTags() {
+    return nerStandardTags;
+  }
+
+  public void setNerStandardTags(List<NerStandardTagsItem> nerStandardTags) {
+    this.nerStandardTags = nerStandardTags;
+  }
+}
