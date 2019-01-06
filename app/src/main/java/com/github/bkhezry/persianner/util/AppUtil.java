f1bf1a54d5e6534124d942d04890b00f3f536300
Behrouz Khezry <bkhezry@gmail.com>
2019-01-06 14:23:32 +0330

    add getLoadingDialog function to AppUtil class.

--- app/src/main/java/com/github/bkhezry/persianner/util/AppUtil.java
+++ app/src/main/java/com/github/bkhezry/persianner/util/AppUtil.java
@@ -1,9 +1,32 @@
 package com.github.bkhezry.persianner.util;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.github.bkhezry.persianner.R;
+
 public class AppUtil {
   private static final Long timeDiff = 1800000L;
 
   public static boolean isTokenExpire(Long storeTimestamp) {
     return System.currentTimeMillis() - storeTimestamp > timeDiff;
+  }
+
+  public static Dialog getLoadingDialog(Context context) {
+    Dialog dialog = new Dialog(context);
+    dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+    dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+    dialog.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    dialog.setContentView(R.layout.dialog_full_screen_loading);
+    dialog.setCancelable(false);
+    WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+    lp.copyFrom(dialog.getWindow().getAttributes());
+    lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+    lp.height = WindowManager.LayoutParams.MATCH_PARENT;
+    dialog.getWindow().setAttributes(lp);
+    return dialog;
   }
 }
