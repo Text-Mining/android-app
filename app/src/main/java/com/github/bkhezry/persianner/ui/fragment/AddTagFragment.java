a39ed316cbe2f8cbd71470238a72b3f14633b01a
Behrouz Khezry <bkhezry@gmail.com>
2019-01-06 18:46:39 +0330

    add UI layout of add tag fragment.

--- app/src/main/java/com/github/bkhezry/persianner/ui/fragment/AddTagFragment.java
+++ app/src/main/java/com/github/bkhezry/persianner/ui/fragment/AddTagFragment.java
@@ -1,6 +1,44 @@
 package com.github.bkhezry.persianner.ui.fragment;
 
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
+import com.github.bkhezry.persianner.R;
+
+import butterknife.ButterKnife;
+
 public class AddTagFragment extends DialogFragment {
+
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.fragment_add_tag,
+        container, false);
+    ButterKnife.bind(this, view);
+    return view;
+  }
+
+  @NonNull
+  @Override
+  public Dialog onCreateDialog(Bundle savedInstanceState) {
+    Dialog dialog = super.onCreateDialog(savedInstanceState);
+    dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+    dialog.setCancelable(true);
+    WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+    lp.copyFrom(dialog.getWindow().getAttributes());
+    lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+    lp.height = WindowManager.LayoutParams.MATCH_PARENT;
+    dialog.getWindow().setAttributes(lp);
+    return dialog;
+  }
+
+
 }
