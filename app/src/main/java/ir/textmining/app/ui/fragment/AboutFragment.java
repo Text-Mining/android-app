1e283ed80217b3ae43c2bec9d97933d1ce440b1e
Behrouz Khezry <bkhezry@gmail.com>
2019-01-09 22:13:34 +0330

    show webs url in the browser

--- app/src/main/java/ir/textmining/app/ui/fragment/AboutFragment.java
+++ app/src/main/java/ir/textmining/app/ui/fragment/AboutFragment.java
@@ -2,7 +2,10 @@ package ir.textmining.app.ui.fragment;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,10 +17,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.widget.AppCompatTextView;
 import androidx.fragment.app.DialogFragment;
 
+import com.blankj.utilcode.util.SnackbarUtils;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import ir.textmining.app.R;
+import ir.textmining.app.util.AppUtil;
+import ir.textmining.app.util.Constant;
 
 public class AboutFragment extends DialogFragment {
   @BindView(R.id.title)
@@ -73,11 +80,25 @@ public class AboutFragment extends DialogFragment {
   void handleLayoutClick(View view) {
     switch (view.getId()) {
       case R.id.source_code_layout:
+        startBrowser(Constant.GITHUB_URL, view);
         break;
       case R.id.website_layout:
+        startBrowser(Constant.WEB_SITE_URL, view);
         break;
       case R.id.developer_layout:
+        startBrowser(Constant.DEVELOPER_URL, view);
         break;
     }
+  }
+
+  private void startBrowser(String url, View view) {
+    try {
+      Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+      startActivity(browserIntent);
+    } catch (ActivityNotFoundException e) {
+      AppUtil.showSnackbar(view, getString(R.string.browser_not_found_label), activity, SnackbarUtils.LENGTH_INDEFINITE);
+      e.printStackTrace();
+    }
+
   }
 }
