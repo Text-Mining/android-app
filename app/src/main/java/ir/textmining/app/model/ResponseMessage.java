0b276df826dc545f6b1981d586d1f06eb312492c
Behrouz Khezry <bkhezry@gmail.com>
2019-01-09 20:31:47 +0330

    add message field to ResponseMessage class.

--- app/src/main/java/ir/textmining/app/model/ResponseMessage.java
+++ app/src/main/java/ir/textmining/app/model/ResponseMessage.java
@@ -1,4 +1,16 @@
 package ir.textmining.app.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class ResponseMessage {
+  @SerializedName("message")
+  private String message;
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
 }
