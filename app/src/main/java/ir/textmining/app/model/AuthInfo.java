03e03ec30e2654c688e7fa037418ec421e2c59f4
Behrouz Khezry <bkhezry@gmail.com>
2019-01-12 16:36:39 +0330

    add token validate duration field to auth info.

--- app/src/main/java/ir/textmining/app/model/AuthInfo.java
+++ app/src/main/java/ir/textmining/app/model/AuthInfo.java
@@ -6,10 +6,9 @@ public class AuthInfo {
 
   @SerializedName("token")
   private String token;
-
   private String email;
-
   private Long storeTimestamp;
+  private Long tokenValidDuration;
 
   public String getToken() {
     return token;
@@ -33,5 +32,13 @@ public class AuthInfo {
 
   public void setStoreTimestamp(Long storeTimestamp) {
     this.storeTimestamp = storeTimestamp;
+  }
+
+  public Long getTokenValidDuration() {
+    return tokenValidDuration;
+  }
+
+  public void setTokenValidDuration(Long tokenValidDuration) {
+    this.tokenValidDuration = tokenValidDuration;
   }
 }
