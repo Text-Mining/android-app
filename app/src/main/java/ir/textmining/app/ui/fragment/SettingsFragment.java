e5b9588f39e6631a91d894539a2b361ab982f639
Behrouz Khezry <bkhezry@gmail.com>
2019-01-12 16:08:59 +0330

    create settings fragment.

---
+++ app/src/main/java/ir/textmining/app/ui/fragment/SettingsFragment.java
@@ -0,0 +1,53 @@
+package ir.textmining.app.ui.fragment;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import ir.textmining.app.R;
+
+public class SettingsFragment extends DialogFragment {
+  private Activity activity;
+
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.fragment_settings,
+        container, false);
+    ButterKnife.bind(this, view);
+    activity = getActivity();
+    return view;
+  }
+
+  @NonNull
+  @Override
+  public Dialog onCreateDialog(Bundle savedInstanceState) {
+    Dialog dialog = super.onCreateDialog(savedInstanceState);
+    dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+    dialog.setCancelable(true);
+    WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+    lp.copyFrom(dialog.getWindow().getAttributes());
+    lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+    lp.height = WindowManager.LayoutParams.MATCH_PARENT;
+    dialog.getWindow().setAttributes(lp);
+    return dialog;
+  }
+
+  @OnClick(R.id.close_button)
+  void close() {
+    dismiss();
+    if (getFragmentManager() != null) {
+      getFragmentManager().popBackStack();
+    }
+  }
+}
