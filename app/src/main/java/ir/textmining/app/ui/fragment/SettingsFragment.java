5d7137eed2917eca01744cc7cb8cdff963550076
Behrouz Khezry <bkhezry@gmail.com>
2019-02-10 01:52:20 +0330

    Revert "implement dark theme"

--- app/src/main/java/ir/textmining/app/ui/fragment/SettingsFragment.java
+++ app/src/main/java/ir/textmining/app/ui/fragment/SettingsFragment.java
@@ -2,20 +2,15 @@ package ir.textmining.app.ui.fragment;
 
 import android.app.Activity;
 import android.app.Dialog;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.CompoundButton;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.AppCompatTextView;
-import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.DialogFragment;
 
 import com.github.pwittchen.prefser.library.rx2.Prefser;
@@ -27,18 +22,12 @@ import ir.textmining.app.R;
 import ir.textmining.app.listener.SettingsEventListener;
 import ir.textmining.app.model.AuthInfo;
 import ir.textmining.app.util.Constant;
-import ir.textmining.app.util.SettingsManager;
 
 public class SettingsFragment extends DialogFragment {
   @BindView(R.id.email_text_view)
   AppCompatTextView emailTextView;
-
-  @BindView(R.id.dark_theme_switch)
-  SwitchCompat darkThemeSwitch;
-
   private Activity activity;
   private SettingsEventListener listener;
-  private SharedPreferences prefs;
 
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -47,25 +36,7 @@ public class SettingsFragment extends DialogFragment {
         container, false);
     ButterKnife.bind(this, view);
     initVariables();
-    setSwitchButtonListener();
-
     return view;
-  }
-
-  private void setSwitchButtonListener() {
-    darkThemeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-      @Override
-      public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
-
-        SettingsManager.getInstance(activity).setDarkThemeEnabled(checked);
-        if (checked)
-          AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-        else
-          AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-        activity.recreate();
-
-      }
-    });
   }
 
   private void initVariables() {
@@ -75,7 +46,6 @@ public class SettingsFragment extends DialogFragment {
     if (authInfo != null) {
       emailTextView.setText(authInfo.getEmail());
     }
-    darkThemeSwitch.setChecked(SettingsManager.getInstance(activity).isDarkThemeEnabled());
   }
 
   @NonNull
--- app/src/main/java/ir/textmining/app/util/Constant.java
+++ app/src/main/java/ir/textmining/app/util/Constant.java
@@ -5,7 +5,6 @@ public class Constant {
   public static final String AUTH_INFO = "auth-info";
   public static final String SIGN_UP_URL = "https://app.text-mining.ir/Account/Register";
   public static final String LANGUAGE = "language";
-  public static final String DARK_THEME = "dark-theme";
   public static final String GITHUB_URL = "https://github.com/Text-Mining/";
   public static final String WEB_SITE_URL = "https://app.text-mining.ir/";
   public static final String DEVELOPER_URL = "https://twitter.com/bkhezry";
