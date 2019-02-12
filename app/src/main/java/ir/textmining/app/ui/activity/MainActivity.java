f5c136ada997ccb5050b7058963db8c7189becc4
Behrouz Khezry <bkhezry@gmail.com>
2019-02-12 18:58:05 +0330

    set text color of tagged token with 2 diff color.

--- app/src/main/java/ir/textmining/app/ui/activity/MainActivity.java
+++ app/src/main/java/ir/textmining/app/ui/activity/MainActivity.java
@@ -50,202 +50,204 @@ import retrofit2.Response;
 
 public class MainActivity extends BaseActivity implements SettingsEventListener {
 
-  @BindView(R.id.chip_group)
-  ChipGroup chipGroup;
-  @BindView(R.id.coordinatorLayout)
-  CoordinatorLayout coordinatorLayout;
-  @BindView(R.id.bar)
-  BottomAppBar bar;
-  @BindView(R.id.navigation_view)
-  NavigationView navigationView;
-  private BottomSheetBehavior bottomDrawerBehavior;
-  private AuthInfo authInfo;
-  private Box<NerStandardTagsItem> tagsItemBox;
-  private SkeletonScreen skeletonScreen;
+    @BindView(R.id.chip_group)
+    ChipGroup chipGroup;
+    @BindView(R.id.coordinatorLayout)
+    CoordinatorLayout coordinatorLayout;
+    @BindView(R.id.bar)
+    BottomAppBar bar;
+    @BindView(R.id.navigation_view)
+    NavigationView navigationView;
+    private BottomSheetBehavior bottomDrawerBehavior;
+    private AuthInfo authInfo;
+    private Box<NerStandardTagsItem> tagsItemBox;
+    private SkeletonScreen skeletonScreen;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    ButterKnife.bind(this);
-    subscribeFirebaseTopic();
-    setSupportActionBar(bar);
-    initBottomNavigationView();
-    initVariables();
-    getSentence();
-  }
-
-  private void subscribeFirebaseTopic() {
-    FirebaseMessaging.getInstance().subscribeToTopic("update_app")
-        .addOnCompleteListener(new OnCompleteListener<Void>() {
-          @Override
-          public void onComplete(@NonNull Task<Void> task) {
-
-          }
-        });
-  }
-
-  private void initBottomNavigationView() {
-    View bottomDrawer = coordinatorLayout.findViewById(R.id.bottom_drawer);
-    bottomDrawerBehavior = BottomSheetBehavior.from(bottomDrawer);
-    bottomDrawerBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
-    bar.setNavigationOnClickListener(
-        new View.OnClickListener() {
-          @Override
-          public void onClick(View v) {
-            bottomDrawerBehavior.setState(BottomSheetBehavior.STATE_HALF_EXPANDED);
-          }
-        });
-    bar.setNavigationIcon(R.drawable.ic_menu_black_24dp);
-    navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-      @Override
-      public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        if (menuItem.getItemId() == R.id.about_item) {
-          showAboutFragment();
-        } else if (menuItem.getItemId() == R.id.settings_item) {
-          showSettingsFragment();
-        }
-        hideBottomDrawer();
-        return false;
-      }
-    });
-
-  }
-
-  private void showAboutFragment() {
-    AppUtil.showFragment(new AboutFragment(), getSupportFragmentManager());
-  }
-
-  private void showSettingsFragment() {
-    SettingsFragment settingsFragment = new SettingsFragment();
-    AppUtil.showFragment(settingsFragment, getSupportFragmentManager());
-  }
-
-  private void hideBottomDrawer() {
-    bottomDrawerBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
-  }
-
-  private void initVariables() {
-    Prefser prefser = new Prefser(this);
-    authInfo = prefser.get(Constant.AUTH_INFO, AuthInfo.class, null);
-    BoxStore boxStore = MyApplication.getBoxStore();
-    tagsItemBox = boxStore.boxFor(NerStandardTagsItem.class);
-  }
-
-  private void getSentence() {
-    if (NetworkUtils.isConnected()) {
-      requestGetSentence();
-    } else {
-      AppUtil.showSnackbar(coordinatorLayout, getString(R.string.no_internet_label), this, SnackbarUtils.LENGTH_LONG);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        subscribeFirebaseTopic();
+        setSupportActionBar(bar);
+        initBottomNavigationView();
+        initVariables();
+        getSentence();
     }
-  }
 
-  private void requestGetSentence() {
-    showSkeleton();
-    APIService apiService = RetrofitUtil.getRetrofit(authInfo.getToken()).create(APIService.class);
-    Call<Sentence> call = apiService.randomSentence();
-    call.enqueue(new Callback<Sentence>() {
-      @Override
-      public void onResponse(@NonNull Call<Sentence> call, @NonNull Response<Sentence> response) {
-        skeletonScreen.hide();
-        if (response.isSuccessful()) {
-          Sentence sentence = response.body();
-          if (sentence != null) {
-            if (sentence.getWords().size() == 0) {
-              getSentence();
-            } else {
-              handleSentence(sentence);
+    private void subscribeFirebaseTopic() {
+        FirebaseMessaging.getInstance().subscribeToTopic("update_app")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                    }
+                });
+    }
+
+    private void initBottomNavigationView() {
+        View bottomDrawer = coordinatorLayout.findViewById(R.id.bottom_drawer);
+        bottomDrawerBehavior = BottomSheetBehavior.from(bottomDrawer);
+        bottomDrawerBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+        bar.setNavigationOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        bottomDrawerBehavior.setState(BottomSheetBehavior.STATE_HALF_EXPANDED);
+                    }
+                });
+        bar.setNavigationIcon(R.drawable.ic_menu_black_24dp);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if (menuItem.getItemId() == R.id.about_item) {
+                    showAboutFragment();
+                } else if (menuItem.getItemId() == R.id.settings_item) {
+                    showSettingsFragment();
+                }
+                hideBottomDrawer();
+                return false;
             }
-          }
-        } else if (response.code() == 401) {
-          tokenInvalid();
-        }
-      }
+        });
 
-      @Override
-      public void onFailure(@NonNull Call<Sentence> call, @NonNull Throwable t) {
-        skeletonScreen.hide();
-        t.printStackTrace();
-        AppUtil.showSnackbar(chipGroup, getString(R.string.retry_request_message), MainActivity.this, SnackbarUtils.LENGTH_LONG);
-      }
-    });
-  }
-
-  private void tokenInvalid() {
-    Intent intent = new Intent(this, LauncherActivity.class);
-    intent.putExtra(Constant.TOKEN_INVALID, true);
-    startActivity(intent);
-    finish();
-  }
-
-  private void handleSentence(Sentence sentence) {
-    chipGroup.removeAllViews();
-    for (WordsItem wordsItem : sentence.getWords()) {
-      Chip chip = generateClip(wordsItem, sentence.getSentenceId());
-      chipGroup.addView(chip);
     }
-  }
 
-  private Chip generateClip(WordsItem wordsItem, String sentenceId) {
-    Chip chip =
-        (Chip) getLayoutInflater().inflate(R.layout.item_chip, chipGroup, false);
-    chip.setText(wordsItem.getWord());
-    chip.setChipBackgroundColor(ColorStateList.valueOf(Color.parseColor(AppUtil.colorHexString(wordsItem.getTag(), tagsItemBox))));
-    if (!wordsItem.getTag().equals("O")) {
-      chip.setTextColor(Color.WHITE);
+    private void showAboutFragment() {
+        AppUtil.showFragment(new AboutFragment(), getSupportFragmentManager());
     }
-    chip.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        selectTagFragment(chip, wordsItem, sentenceId);
-      }
-    });
-    return chip;
-  }
 
-  private void selectTagFragment(Chip chip, WordsItem wordsItem, String sentenceId) {
-    SelectTagFragment fragment = new SelectTagFragment();
-    fragment.setWordData(wordsItem, sentenceId);
-    fragment.setListener(new SelectTagEventListener() {
-      @Override
-      public void tagSuccessEvent(String tagTitle) {
-        AppUtil.showSnackbar(chipGroup, getString(R.string.tag_success_message), MainActivity.this, SnackbarUtils.LENGTH_LONG);
-        wordsItem.setTag(tagTitle);
-        chip.setChipBackgroundColor(ColorStateList.valueOf(Color.parseColor(AppUtil.colorHexString(tagTitle, tagsItemBox))));
-        if (!tagTitle.equals("O")) {
-          chip.setTextColor(Color.WHITE);
+    private void showSettingsFragment() {
+        SettingsFragment settingsFragment = new SettingsFragment();
+        AppUtil.showFragment(settingsFragment, getSupportFragmentManager());
+    }
+
+    private void hideBottomDrawer() {
+        bottomDrawerBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+    }
+
+    private void initVariables() {
+        Prefser prefser = new Prefser(this);
+        authInfo = prefser.get(Constant.AUTH_INFO, AuthInfo.class, null);
+        BoxStore boxStore = MyApplication.getBoxStore();
+        tagsItemBox = boxStore.boxFor(NerStandardTagsItem.class);
+    }
+
+    private void getSentence() {
+        if (NetworkUtils.isConnected()) {
+            requestGetSentence();
+        } else {
+            AppUtil.showSnackbar(coordinatorLayout, getString(R.string.no_internet_label), this, SnackbarUtils.LENGTH_LONG);
         }
-      }
+    }
 
-      @Override
-      public void tokenInvalidEvent() {
+    private void requestGetSentence() {
+        showSkeleton();
+        APIService apiService = RetrofitUtil.getRetrofit(authInfo.getToken()).create(APIService.class);
+        Call<Sentence> call = apiService.randomSentence();
+        call.enqueue(new Callback<Sentence>() {
+            @Override
+            public void onResponse(@NonNull Call<Sentence> call, @NonNull Response<Sentence> response) {
+                skeletonScreen.hide();
+                if (response.isSuccessful()) {
+                    Sentence sentence = response.body();
+                    if (sentence != null) {
+                        if (sentence.getWords().size() == 0) {
+                            getSentence();
+                        } else {
+                            handleSentence(sentence);
+                        }
+                    }
+                } else if (response.code() == 401) {
+                    tokenInvalid();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Sentence> call, @NonNull Throwable t) {
+                skeletonScreen.hide();
+                t.printStackTrace();
+                AppUtil.showSnackbar(chipGroup, getString(R.string.retry_request_message), MainActivity.this, SnackbarUtils.LENGTH_LONG);
+            }
+        });
+    }
+
+    private void tokenInvalid() {
+        Intent intent = new Intent(this, LauncherActivity.class);
+        intent.putExtra(Constant.TOKEN_INVALID, true);
+        startActivity(intent);
+        finish();
+    }
+
+    private void handleSentence(Sentence sentence) {
+        chipGroup.removeAllViews();
+        for (WordsItem wordsItem : sentence.getWords()) {
+            Chip chip = generateClip(wordsItem, sentence.getSentenceId());
+            chipGroup.addView(chip);
+        }
+    }
+
+    private Chip generateClip(WordsItem wordsItem, String sentenceId) {
+        Chip chip =
+                (Chip) getLayoutInflater().inflate(R.layout.item_chip, chipGroup, false);
+        chip.setText(wordsItem.getWord());
+        chip.setChipBackgroundColor(ColorStateList.valueOf(Color.parseColor(AppUtil.colorHexString(wordsItem.getTag(), tagsItemBox))));
+        if (!wordsItem.getTag().equals("O")) {
+            chip.setTextColor(Color.WHITE);
+        }
+        chip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectTagFragment(chip, wordsItem, sentenceId);
+            }
+        });
+        return chip;
+    }
+
+    private void selectTagFragment(Chip chip, WordsItem wordsItem, String sentenceId) {
+        SelectTagFragment fragment = new SelectTagFragment();
+        fragment.setWordData(wordsItem, sentenceId);
+        fragment.setListener(new SelectTagEventListener() {
+            @Override
+            public void tagSuccessEvent(String tagTitle) {
+                AppUtil.showSnackbar(chipGroup, getString(R.string.tag_success_message), MainActivity.this, SnackbarUtils.LENGTH_LONG);
+                wordsItem.setTag(tagTitle);
+                chip.setChipBackgroundColor(ColorStateList.valueOf(Color.parseColor(AppUtil.colorHexString(tagTitle, tagsItemBox))));
+                if (!tagTitle.equals("O")) {
+                    chip.setTextColor(Color.WHITE);
+                } else {
+                    chip.setTextColor(Color.BLACK);
+                }
+            }
+
+            @Override
+            public void tokenInvalidEvent() {
+                tokenInvalid();
+            }
+        });
+        AppUtil.showFragment(fragment, getSupportFragmentManager());
+    }
+
+    @OnClick(R.id.next_sentence_button)
+    public void nextSentence() {
+        getSentence();
+    }
+
+    @OnClick(R.id.legend_button)
+    public void showLegend() {
+        AppUtil.showFragment(new TagLegendFragment(), getSupportFragmentManager());
+    }
+
+    private void showSkeleton() {
+        skeletonScreen = Skeleton.bind(chipGroup)
+                .shimmer(true)
+                .angle(0)
+                .color(R.color.shimmer_color)
+                .load(R.layout.item_skeleton)
+                .show();
+    }
+
+    @Override
+    public void signOut() {
         tokenInvalid();
-      }
-    });
-    AppUtil.showFragment(fragment, getSupportFragmentManager());
-  }
-
-  @OnClick(R.id.next_sentence_button)
-  public void nextSentence() {
-    getSentence();
-  }
-
-  @OnClick(R.id.legend_button)
-  public void showLegend() {
-    AppUtil.showFragment(new TagLegendFragment(), getSupportFragmentManager());
-  }
-
-  private void showSkeleton() {
-    skeletonScreen = Skeleton.bind(chipGroup)
-        .shimmer(true)
-        .angle(0)
-        .color(R.color.shimmer_color)
-        .load(R.layout.item_skeleton)
-        .show();
-  }
-
-  @Override
-  public void signOut() {
-    tokenInvalid();
-  }
+    }
 }
