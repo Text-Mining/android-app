229a49829d3de1901bfd8a2a84b5d773fe641f98
Behrouz Khezry <bkhezry@gmail.com>
2019-03-19 17:02:35 +0330

    create App intro activity

---
+++ app/src/main/java/ir/textmining/app/ui/activity/AppIntroActivity.java
@@ -0,0 +1,14 @@
+package ir.textmining.app.ui.activity;
+
+import android.os.Bundle;
+
+import com.github.paolorotolo.appintro.AppIntro2;
+
+public class AppIntroActivity extends AppIntro2 {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
